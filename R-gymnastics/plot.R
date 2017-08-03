a96991cca66964af16211924e5ca7ee94ea60e93
John Pretz <john.pretz@gmail.com>
2017-08-02 21:29:43 -0400

    dump to file

--- R-gymnastics/plot.R
+++ R-gymnastics/plot.R
@@ -2,7 +2,8 @@ library("RSQLite")
 mydb <- dbConnect(RSQLite::SQLite(), "data.db")
 dbListTables(mydb)
 data <- dbGetQuery(mydb, 'SELECT * FROM xvy')
+
+
+png('data.png')
 plot(data$x,data$y)
 
-dev.copy(png,'data.png')
-dev.off()
