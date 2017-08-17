a01860ab2669f637f2e507e17d7dcc20e3b9a8cd
John Pretz <john.pretz@gmail.com>
2017-08-17 16:12:19 -0400

    working example

--- js-xml/parse.js
+++ js-xml/parse.js
@@ -1,3 +1,28 @@
 main = function(){
   console.log("hello");
+  loadXMLDoc();
+
+}
+
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xmlhttp.open("GET", "data.xml", true);
+  xmlhttp.send();
+}
+
+function myFunction(xml) {
+  var x, i, xmlDoc, txt;
+  xmlDoc = xml.responseXML;
+  txt = "";
+  x = xmlDoc.getElementsByTagName("x");
+  y = xmlDoc.getElementsByTagName("y");
+  for (i = 0; i< x.length; i++) {
+    txt += x[i].childNodes[0].nodeValue + "," + y[i].childNodes[0].nodeValue + "<br>";
+  }
+  document.getElementById("demo").innerHTML = txt;
 }
