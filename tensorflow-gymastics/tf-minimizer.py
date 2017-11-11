b2e0ddf3aa4694fc50e429a8e0771bd4ea5b28c9
John Pretz <john.pretz@gmail.com>
2017-11-10 19:36:30 -0500

    minimizer script

---
+++ tensorflow-gymastics/tf-minimizer.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+sess = tf.Session()
+
+x = tf.Variable(5.0)
+y = tf.Variable(-5.0)
+
+f = x * x + y * y
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+sess.run(f)
+
+updates = tf.train.GradientDescentOptimizer(0.3).minimize(f,var_list=[x])
+
+for i in range(0,50):
+    sess.run(updates)
+    print(sess.run(f),sess.run(x),sess.run(y))
